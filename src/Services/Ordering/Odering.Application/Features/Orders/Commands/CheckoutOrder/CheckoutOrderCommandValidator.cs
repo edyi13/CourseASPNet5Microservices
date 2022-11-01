a7296618f836821812d8166d563d741b9817face
Edson MZ <emz19.com@gmail.com>
2022-10-31 19:52:52 -0600

    Added UpdateOrder and DeleteOrder use cases

--- src/Services/Ordering/Odering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
+++ src/Services/Ordering/Odering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -23,5 +23,5 @@ namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
                 .NotEmpty().WithMessage("{TotalPrice} is required.")
                 .GreaterThan(0).WithMessage("{TotalPrice} should be greater than zero.");
         }
-    }
+    } 
 }
